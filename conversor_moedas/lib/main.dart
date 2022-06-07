806e6f07c266d0b993c440e8dd4ce737c2c9faf2
bronsilber <bron.teixeira.s@gmail.com.br>
2022-06-07 18:36:39 -0300

    Chamda da API para consumo

--- conversor_moedas/lib/main.dart
+++ conversor_moedas/lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:async/async.dart';
+import 'dart:convert';
 
 const requestURL = "https://api.hgbrasil.com/finance?format=json-cors&key=8e0b83f7";
 
 void main() async{
 
   http.Response response = await http.get(requestURL);
+  print( json.decode(response.body));
+
 
   runApp(MaterialApp(
     home: Container(),
